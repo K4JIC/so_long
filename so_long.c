3609e22fe77f790e9f7805acfaa028561b61353b
K4JIC <k4jic@DESKTOP-RAV1T6J.localdomain>
2025-12-02 09:54:57 +0900

    2025-12-02

--- so_long.c
+++ so_long.c
@@ -6,17 +6,36 @@
 /*   By: tozaki <tozaki@student.42.jp>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 20:02:06 by tozaki            #+#    #+#             */
-/*   Updated: 2025/11/29 20:49:58 by tozaki           ###   ########.fr       */
+/*   Updated: 2025/12/02 03:36:17 by tozaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include "mlx.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	deal_key(int key, void *param)
+{
+	if (param)
+		(void)key;
+	ft_putchar('x');
+	return (0);
+}
 
 int	main(void)
 {
 	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*img_ptr;
 
 	mlx_ptr = mlx_init();
-	mlx_new_window(mlx_ptr, 300, 900, "test");
+	win_ptr = mlx_new_window(mlx_ptr, 400, 300, "test");
+	img_ptr = mlx_new_image(mlx_ptr, 100, 100);
+	mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 100, 100);
+	mlx_key_hook(win_ptr, deal_key, (void *)0);
 	mlx_loop(mlx_ptr);
 }
